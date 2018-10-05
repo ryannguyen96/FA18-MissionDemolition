0f7a7e9971a2f569cfb9e69dc436e0de7ec6a59b
Ryan Nguyen <shiga2714@gmail.com>
2018-10-05 15:02:58 -0400

    Added Slingshot.cs
    - Slingshot script created with OnMouseEnter and OnMouseExit.
    - Script is tied to Slingshot object.

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+
+
+    void OnMouseEnter()
+    {
+        print("Slingshot:OnMouseEnter()");
+    }
+
+    void OnMouseExit()
+    {
+        print("Slingshot:OnMouseExit()");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
