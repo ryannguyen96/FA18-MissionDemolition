f093fcd23dc8076846b35932f740648418d77eb2
Ryan Nguyen <shiga2714@gmail.com>
2018-10-05 17:48:16 -0400

    Can now shoot after Projectile is asleep
    - Projectiles going to sleep will pan camera back to Slingshot.

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -22,10 +22,34 @@ public class FollowCam : MonoBehaviour {
     {
         // If there is only one line following an if, it doesn't need braces
 
-        if (POI == null) return; // return if there is no poi
+        //if (POI == null) return; // return if there is no poi
 
         // Get the position of poi
-        Vector3 destination = POI.transform.position;
+        //Vector3 destination = POI.transform.position;
+
+        Vector3 destination;
+
+        // If there is no POI, return to P:[0,0,0]
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            // Get the position of POI
+            destination = POI.transform.position;
+            // If POI is a Projectile, check to see if it's at rest
+            if(POI.tag == "Projectile")
+            {
+                // If it is sleeping, not moving
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    // return to default view
+                    POI = null;
+                    return;
+                }
+            }
+        }
 
         // Limit the X & Y to minimum values
         destination.x = Mathf.Max(minXY.x, destination.x);
